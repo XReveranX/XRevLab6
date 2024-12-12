6e3d7b44683ab5a6a3d781e3013d89039c6fce4a
XReveranX <94445439+XReveranX@users.noreply.github.com>
2024-12-12 15:59:31 +0500

    Update num1.py

--- num1.py
+++ num1.py
@@ -1,11 +1,12 @@
 import itertools
 
 def variant(kl):
+    kl=kl+kl
     k=0
-    l1=itertools.permutations(kl)
+    l1=itertools.combinations_with_replacement(kl,6)
     l2=list(l1)
     for i in range(len(l2)):
-        if (l2[i][0] == "Р") and (l2[i][4] == "К"):
+        if (l2[i][0] == "Р") and (l2[i][5] == "К"):
             k+=1
     print(l2)  #Добавил для проверки
     return(k)
